a2fd23cececa0286fec33065eac1cfcd44905f6a
soufiane <81969518+02soufiane@users.noreply.github.com>
2023-11-25 02:13:48 +0100

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -1,13 +1,13 @@
 -- Команди створення таблиць
-CREATE TABLE UserInformation (
-    UserID INT PRIMARY KEY,
+CREATE TABLE ConsumerInformation (
+    ConsumerID INT PRIMARY KEY,
     Name VARCHAR(255) NOT NULL,
     Email VARCHAR(255) UNIQUE,
     Age SMALLINT CHECK (Age >= 0 AND Age <= 99)
 );
 
-CREATE TABLE User (
-    UserID INT PRIMARY KEY,
+CREATE TABLE Consumer (
+    ConsumerID INT PRIMARY KEY,
     Login VARCHAR(20) CHECK (LENGTH(Login) < 20),
     Password VARCHAR(20) CHECK (LENGTH(Password) < 20)
 );
@@ -33,7 +33,7 @@ CREATE TABLE HealthAnswer (
 
 CREATE TABLE HealthData (
     DataID INT PRIMARY KEY,
-    UserID INT,
+    ConsumerID INT,
     Date DATE CHECK (Date <= CURRENT_DATE),
     HealthDataText TEXT CHECK (LENGTH(HealthDataText) < 500),
     FOREIGN KEY (UserID) REFERENCES User(UserID)
