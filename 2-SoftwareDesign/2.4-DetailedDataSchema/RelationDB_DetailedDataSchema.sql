5d75911dd392462e728161c8534bbf48d4ec5d4d
soufiane <81969518+02soufiane@users.noreply.github.com>
2023-11-25 03:22:15 +0200

    Add files via upload

---
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -0,0 +1,40 @@
+-- Команди створення таблиць
+CREATE TABLE ConsumerInformation (
+    ConsumerID INT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) UNIQUE,
+    Age SMALLINT CHECK (Age >= 0 AND Age <= 99)
+);
+
+CREATE TABLE Consumer (
+    ConsumerID INT PRIMARY KEY,
+    Login VARCHAR(20) CHECK (LENGTH(Login) < 20),
+    Password VARCHAR(20) CHECK (LENGTH(Password) < 20)
+);
+
+CREATE TABLE HealthCenterEmployee (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Age SMALLINT CHECK (Age >= 0 AND Age <= 99),
+    Position VARCHAR(255) CHECK (LENGTH(Position) > 5)
+);
+
+CREATE TABLE HealthQuestion (
+    QuestionID INT PRIMARY KEY,
+    QuestionText TEXT CHECK (LENGTH(QuestionText) > 0 AND LENGTH(QuestionText) < 500),
+    Date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE HealthAnswer (
+    AnswerID INT PRIMARY KEY,
+    AnswerText TEXT CHECK (LENGTH(AnswerText) > 0 AND LENGTH(AnswerText) < 500),
+    Date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE HealthData (
+    DataID INT PRIMARY KEY,
+    ConsumerID INT,
+    Date DATE CHECK (Date <= CURRENT_DATE),
+    HealthDataText TEXT CHECK (LENGTH(HealthDataText) < 500),
+    FOREIGN KEY (UserID) REFERENCES User(UserID)
+);
---
+++ 2-SoftwareDesign/2.5-UMLProgramClasses/README.md
@@ -0,0 +1,3 @@
+### UML-діаграма програмних класів
+
+![UMLProgramClasses](https://github.com/oleksandrblazhko/ai-211-el/assets/81969518/ad6fdc95-76d5-4cc0-ad35-50d37e3f39f5)
