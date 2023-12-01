60bb3edc5991af22a27f4c3558015b22cb3e2216
soufiane <81969518+02soufiane@users.noreply.github.com>
2023-12-01 01:17:38 +0100

    Update Consumer.kt

--- 3-SoftwareConstruction/3-Program-Сlasses-Example/Consumer.kt
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example/Consumer.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class User(
+data class Consumer(
     var ConsumerId: String? = null,
     var login: String? = null,
     var password: String? = null
